8fe4fdcde5bdc5b94f62168ca125dea35a5b7692
jostein <jostein.l@harrang.net>
2024-03-09 21:37:12 +0100

    new commit

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Login from './Components/Login';
 import Register from './Components/Registrer';
-import 'App.css';
+import './App.css';
 
 function App() {
   return (
